7038e66c52f207ed1e91211e479fabc179e57fc7
Jip J. Dekker <jip@dekker.one>
2021-01-15 15:43:42 +1100

    Add initial highlights.scm for syntax highlighting

---
+++ misc/tree-sitter-minizinc/queries/highlights.scm
@@ -0,0 +1,114 @@
+; Function calls
+(call name: (identifier) @function)
+; Function definitions
+(function_item name: (identifier) @function)
+(predicate name: (identifier) @function)
+
+; Types
+(primitive_type) @type.builtin
+[
+ "array"
+ "of"
+ "par"
+ "set"
+ "var"
+] @type.builtin
+
+; Identifiers
+(identifier) @variable
+
+; Keywords
+[
+  "annotation"
+  "constraint"
+  "else"
+  "elseif"
+  "endif"
+  "function"
+  "if"
+  "in"
+  "include"
+  "let"
+  "maximize"
+  "minimize"
+  "output"
+  "predicate"
+  "satisfy"
+  "solve"
+  "test"
+  "then"
+  "where"
+] @keyword
+
+; Operators
+[
+  "<->"
+  "->"
+  "<-"
+  "\/"
+  "xor"
+  "/\"
+  "="
+  "=="
+  "!="
+  "<"
+  "<="
+  ">"
+  ">="
+  "in"
+  "subset"
+  "superset"
+  "union"
+  "diff"
+  "symdiff"
+  "intersect"
+  ".."
+  "+"
+  "-"
+  "++"
+  "*"
+  "/"
+  "div"
+  "mod"
+  "^"
+  "::"
+] @operator
+
+; Punctuation
+[
+  ","
+  ":"
+  ";"
+  "|"
+] @punctuation.delimiter
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+; Literals
+[
+  (integer_literal)
+  (float_literal)
+] @number
+
+[
+  (string_literal)
+  (string_interpolation)
+] @string
+(escape_sequence) @escape
+
+[
+  (absent)
+  (boolean_literal)
+] @constant.builtin
+
+; Comments
+[
+  (block_comment)
+  (line_comment)
+] @comment
