47b2d3bd876b9b17183f5060d9607c4baa3d1485
Jip J. Dekker <jip@dekker.one>
2021-01-15 15:28:34 +1100

    Add initial tags.scm for reference jumping

---
+++ misc/tree-sitter-minizinc/queries/tags.scm
@@ -0,0 +1,5 @@
+(function_item name: (identifier) @name) @definition.function
+(predicate name: (identifier) @name) @definition.function
+(annotation name: (identifier) @name) @definition.function
+
+(call name: (identifier) @name) @reference.call
