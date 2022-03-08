fc290f17cf2533fa94db281cb51ef0948236497c
Jip J. Dekker <jip@dekker.one>
2022-03-08 16:25:35 +1100

    Update rust bindings for tree-sitter-minizinc

--- misc/tree-sitter-minizinc/bindings/rust/lib.rs
+++ misc/tree-sitter-minizinc/bindings/rust/lib.rs
@@ -1,22 +1,13 @@
-//! This crate provides a MiniZinc grammar for the [tree-sitter][] parsing library.
+//! This crate provides minizinc language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this grammar to a
+//! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! use tree_sitter::Parser;
-//!
-//! let code = r#"
-//!     var int: x;
-//!     constraint x in {1,3,5};
-//!     output ["x has value \(x)"]
-//! "#;
-//! let mut parser = Parser::new();
-//! parser.set_language(tree_sitter_minizinc::language()).expect("Error loading MiniZinc grammar");
-//! let parsed = parser.parse(code, None);
-//! # let parsed = parsed.unwrap();
-//! # let root = parsed.root_node();
-//! # assert!(!root.has_error());
+//! let code = "";
+//! let mut parser = tree_sitter::Parser::new();
+//! parser.set_language(tree_sitter_minizinc::language()).expect("Error loading minizinc grammar");
+//! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -30,34 +21,32 @@ extern "C" {
 	fn tree_sitter_minizinc() -> Language;
 }
 
-/// Returns the tree-sitter [Language][] for this grammar.
+/// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
 	unsafe { tree_sitter_minizinc() }
 }
 
-/// The source of the MiniZinc tree-sitter grammar description.
-pub const GRAMMAR: &'static str = include_str!("../../grammar.js");
-
-/// The syntax highlighting query for this language.
-pub const HIGHLIGHT_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
-/// The symbol tagging query for this language.
-pub const TAGGING_QUERY: &'static str = include_str!("../../queries/tags.scm");
+// Uncomment these to include any queries that this grammar contains
+
+pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
+// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
+// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
+pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
 	#[test]
-	fn can_load_grammar() {
+	fn test_can_load_grammar() {
 		let mut parser = tree_sitter::Parser::new();
 		parser
 			.set_language(super::language())
-			.expect("Error loading MiniZinc grammar");
+			.expect("Error loading minizinc language");
 	}
 }
