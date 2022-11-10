8158761eef2fddb0c6aec5fb5429efd249bb9080
Jip J. Dekker <jip@dekker.one>
2022-11-10 11:39:36 +1100

    Update legacy interpreter JSON parser to use type information

---
+++ crates/shackle/src/legacy/mod.rs
@@ -0,0 +1,373 @@
+use std::{
+	collections::BTreeMap,
+	io::{BufRead, BufReader},
+	ops::Range,
+	process::{Command, Stdio},
+};
+
+use serde_json::Map;
+
+use crate::{
+	db::CompilerDatabase,
+	error::InternalError,
+	hir::{db::Hir, Identifier},
+	ty::{Ty, TyData},
+	Message, Program, Status, Value,
+};
+
+fn flatten_array(
+	db: &CompilerDatabase,
+	content: &mut Vec<Value>,
+	arr: serde_json::Value,
+	ndim: usize,
+	elem_ty: Ty,
+) -> Result<(), InternalError> {
+	if let serde_json::Value::Array(vec) = arr {
+		if ndim > 1 {
+			for sub_arr in vec {
+				flatten_array(db, content, sub_arr, ndim - 1, elem_ty)?;
+			}
+		} else {
+			for elem in vec {
+				content.push(deserialize_legacy_value(db, elem_ty, elem)?);
+			}
+		}
+		Ok(())
+	} else {
+		Err(InternalError::new(
+			"value from legacy interpreter does not have the expected number of dimensions",
+		))
+	}
+}
+
+pub fn deserialize_legacy_value(
+	db: &CompilerDatabase,
+	ty: Ty,
+	val: serde_json::Value,
+) -> Result<Value, InternalError> {
+	match val {
+		serde_json::Value::Null => {
+			if ty.known_occurs(db) {
+				Err(InternalError::new(
+					format!("legacy interpreter returned an absent value for variable with a type `{}',  known to occur", ty.pretty_print(db)),
+				))
+			} else {
+				Ok(Value::Absent)
+			}
+		}
+		serde_json::Value::Bool(b) => {
+			if let TyData::Boolean(_, _) = ty.lookup(db) {
+				Ok(Value::Boolean(b))
+			} else {
+				Err(InternalError::new(format!(
+					"legacy interpreter returned a Boolean value for variable of type `{}'",
+					ty.pretty_print(db)
+				)))
+			}
+		}
+		serde_json::Value::Number(v) => {
+			if v.is_f64() {
+				if let TyData::Float(_, _) = ty.lookup(db) {
+					Ok(Value::Float(v.as_f64().unwrap()))
+				} else {
+					Err(InternalError::new(format!(
+						"legacy interpreter returned a floating point value for variable of type `{}'",
+						ty.pretty_print(db)
+					)))
+				}
+			} else {
+				assert!(v.is_i64());
+				if let TyData::Integer(_, _) = ty.lookup(db) {
+					Ok(Value::Integer(v.as_i64().unwrap()))
+				} else {
+					Err(InternalError::new(format!(
+						"legacy interpreter returned a integer value for variable of type `{}'",
+						ty.pretty_print(db)
+					)))
+				}
+			}
+		}
+		serde_json::Value::String(v) => {
+			if let TyData::String(_) = ty.lookup(db) {
+				Ok(Value::String(v))
+			} else {
+				Err(InternalError::new(format!(
+					"legacy interpreter returned a String value for variable of type `{}'",
+					ty.pretty_print(db)
+				)))
+			}
+		}
+		serde_json::Value::Array(v) => match ty.lookup(db) {
+			TyData::Array {
+				opt: _,
+				dim,
+				element,
+			} => {
+				if let TyData::Tuple(_, tt) = dim.lookup(db) {
+					// Determine the index sets of the array
+					// FIXME should be returned from the model
+					let mut ranges = Vec::with_capacity(tt.len());
+					let arr = serde_json::Value::Array(v);
+					let mut ii = &arr;
+					while let serde_json::Value::Array(v) = ii {
+						ranges.push(1..(v.len() + 1) as i64);
+						if let Some(fst) = v.last() {
+							ii = fst;
+						} else {
+							ranges.push(1..1);
+						}
+					}
+					// Flatten content
+					let mut content = Vec::new();
+					flatten_array(db, &mut content, arr, tt.len(), element)?;
+
+					Ok(Value::Array(ranges, content))
+				} else {
+					let range: Range<i64> = 1..(v.len() + 1) as i64;
+					let content = v
+						.into_iter()
+						.map(|val| deserialize_legacy_value(db, element, val))
+						.collect::<Result<Vec<_>, _>>()?;
+					Ok(Value::Array(vec![range], content))
+				}
+			}
+			TyData::Tuple(_, types) => {
+				assert_eq!(types.len(), v.len());
+				v.into_iter()
+					.zip(types.iter())
+					.map(|(val, ty)| deserialize_legacy_value(db, *ty, val))
+					.collect::<Result<Vec<_>, _>>()
+					.map(Value::Tuple)
+			}
+			_ => Err(InternalError::new(format!(
+				"legacy interpreter returned a Array value for variable of type `{}'",
+				ty.pretty_print(db)
+			))),
+		},
+		serde_json::Value::Object(mut obj) => match ty.lookup(db) {
+			TyData::Enum(_, _, _) => {
+				let e = if let Some(s) = obj["e"].as_str() {
+					String::from(s)
+				} else if let Some(x) = obj["e"].as_i64() {
+					x.to_string()
+				} else if let Value::Enum(s) = deserialize_legacy_value(db, ty, obj["e"].clone())? {
+					s
+				} else {
+					return Err(InternalError::new(format!(
+						"lagacy interpreter returned an invalid enum value `{:?}'",
+						obj
+					)));
+				};
+				Ok(if obj.contains_key("c") {
+					assert_eq!(obj.len(), 2);
+					Value::Enum(format!("{}({})", obj["c"].as_str().unwrap(), e))
+				} else if obj.contains_key("i") {
+					assert_eq!(obj.len(), 2);
+					Value::Enum(format!("to_enum({}, {})", e, obj["i"].as_i64().unwrap()))
+				} else {
+					assert_eq!(obj.len(), 1);
+					Value::Enum(e)
+				})
+			}
+			TyData::Set(_, _, elem) => {
+				let set = obj.remove("set").unwrap();
+				if let serde_json::Value::Array(set) = set {
+					match elem.lookup(db) {
+						TyData::Integer(_, _) if matches!(set[0], serde_json::Value::Array(_)) => {
+							let mut content = Vec::new();
+							for mem in set {
+								if let serde_json::Value::Array(x) = mem {
+									assert_eq!(x.len(), 2);
+									for i in x[0].as_i64().unwrap()..=x[1].as_i64().unwrap() {
+										content.push(Value::Integer(i))
+									}
+								} else {
+									return Err(InternalError::new(format!(
+										"legacy interpreter invalid range in set members `{}'",
+										mem
+									)));
+								}
+							}
+							Ok(Value::Set(content))
+						}
+						_ => {
+							let content = set
+								.into_iter()
+								.map(|v| deserialize_legacy_value(db, elem, v))
+								.collect::<Result<Vec<_>, _>>()?;
+							Ok(Value::Set(content))
+						}
+					}
+				} else {
+					Err(InternalError::new(format!(
+						"legacy interpreter returned invalid set members `{}'",
+						set
+					)))
+				}
+			}
+			TyData::Record(_, types) => {
+				assert_eq!(types.len(), obj.len());
+				let mut rec = BTreeMap::new();
+				for (name, tt) in types.iter() {
+					let name = name.value(db);
+					let val = deserialize_legacy_value(db, *tt, obj.remove(&name).unwrap())?;
+					rec.insert(name, val);
+				}
+				Ok(Value::Record(rec))
+			}
+			_ => Err(InternalError::new(format!(
+				"legacy interpreter returned a Object value for variable of type `{}'",
+				ty.pretty_print(db)
+			))),
+		},
+	}
+}
+
+impl Program {
+	/// Run the program in the current state
+	/// Solutions are emitted to the callback, and the resulting status is returned.
+	pub fn run<F: Fn(&Message) -> bool>(&mut self, msg_callback: F) -> Status {
+		let mut cmd = Command::new("minizinc");
+		cmd.stdin(Stdio::null())
+			.stdout(Stdio::piped())
+			.stderr(Stdio::null())
+			.arg(self.code.path())
+			.args([
+				"--output-mode",
+				"json",
+				"--json-stream",
+				"--output-time",
+				"--output-objective",
+				"--output-output-item",
+				"--intermediate-solutions",
+				"--solver",
+				self.slv.ident.as_str(),
+			]);
+		if let Some(time_limit) = self.time_limit {
+			cmd.args(["--time-limit", time_limit.as_millis().to_string().as_str()]);
+		}
+		if self.enable_stats {
+			cmd.arg("--statistics");
+		}
+
+		let mut child = cmd.spawn().unwrap(); // TODO: fix unwrap
+		let stdout = child.stdout.take().unwrap();
+
+		let ty_map = self.db.variable_type_map();
+		let mut status = Status::Unknown;
+		for line in BufReader::new(stdout).lines() {
+			match line {
+				Err(err) => {
+					return Status::Err(
+						InternalError::new(format!("minizinc output error: {}", err)).into(),
+					);
+				}
+				Ok(line) => {
+					let mut obj = serde_json::from_str::<Map<String, serde_json::Value>>(&line)
+						.expect("bad message in mzn json");
+					let ty = obj["type"].as_str().expect("bad type field in mzn json");
+					match ty {
+						"statistics" => {
+							if let serde_json::Value::Object(map) = &obj["statistics"] {
+								msg_callback(&Message::Statistic(map));
+							} else {
+								return Status::Err(
+									InternalError::new(format!(
+										"minizinc invalid statistics message: {}",
+										obj["statistics"]
+									))
+									.into(),
+								);
+							}
+						}
+						"solution" => {
+							let mut sol = BTreeMap::new();
+							if let serde_json::Value::Object(map) = obj["output"]
+								.as_object_mut()
+								.unwrap()
+								.remove("json")
+								.unwrap()
+							{
+								for (k, v) in map {
+									let ident = Identifier::new(&k, &self.db);
+									let ty = ty_map[&ident];
+
+									let val = match deserialize_legacy_value(&self.db, ty, v) {
+										Ok(val) => val,
+										Err(e) => return Status::Err(e.into()),
+									};
+									sol.insert(k, val);
+								}
+							} else {
+								panic!("invalid output.json field in mzn json")
+							}
+							msg_callback(&Message::Solution(sol));
+							if let Status::Unknown = status {
+								status = Status::Satisfied;
+							}
+						}
+						"status" => match obj["status"]
+							.as_str()
+							.expect("bad status field in mzn json")
+						{
+							"ALL_SOLUTIONS" => status = Status::AllSolutions,
+							"OPTIMAL_SOLUTION" => status = Status::Optimal,
+							"UNSATISFIABLE" => status = Status::Infeasible,
+							"UNBOUNDED" => todo!(),
+							"UNSAT_OR_UNBOUNDED" => todo!(),
+							"UNKNOWN" => status = Status::Unknown,
+							"ERROR" => {
+								status = Status::Err(
+									InternalError::new(
+										"Error occurred, but no message was provided",
+									)
+									.into(),
+								)
+							}
+							s => {
+								return Status::Err(
+									InternalError::new(format!(
+										"minizinc unknown status type: {}",
+										s
+									))
+									.into(),
+								);
+							}
+						},
+						"error" => {
+							return Status::Err(
+								InternalError::new(format!("minizinc error: {}", obj["message"]))
+									.into(),
+							);
+						}
+						"warning" => {
+							msg_callback(&Message::Warning(
+								obj["message"]
+									.as_str()
+									.expect("invalid message field in mzn json object"),
+							));
+						}
+						s => {
+							return Status::Err(
+								InternalError::new(format!("minizinc unknown message type: {}", s))
+									.into(),
+							);
+						}
+					}
+				}
+			}
+		}
+		match child.wait() {
+			Ok(code) => {
+				if !code.success() {
+					log::warn!(
+						"The MiniZinc process terminated with exit code {}",
+						code.code().unwrap()
+					)
+				};
+				status
+			}
+			Err(e) => Status::Err(InternalError::new(format!("process error: {}", e)).into()),
+		}
+	}
+}
--- crates/shackle/src/lib.rs
+++ crates/shackle/src/lib.rs
@@ -9,15 +9,15 @@ pub mod db;
 pub mod error;
 pub mod file;
 pub mod hir;
+mod legacy;
 pub mod syntax;
 pub mod thir;
 pub mod ty;
 pub mod utils;
 
-use db::{FileReader, Inputs};
-use error::{FileError, InternalError, MultipleErrors, ShackleError};
+use db::{CompilerDatabase, FileReader, Inputs};
+use error::{FileError, MultipleErrors, ShackleError};
 use file::{FileRefData, InputFile};
-use serde::de::{Deserialize, Deserializer, MapAccess, SeqAccess, Visitor};
 use serde_json::Map;
 use tempfile::{Builder, NamedTempFile};
 
@@ -25,10 +25,9 @@ use std::{
 	collections::BTreeMap,
 	env,
 	fmt::{self, Display},
-	io::{BufRead, BufReader, Write},
+	io::Write,
 	ops::Range,
 	path::{Path, PathBuf},
-	process::{Command, Stdio},
 	sync::Arc,
 	time::{Duration, Instant},
 };
@@ -155,6 +154,7 @@ impl Model {
 				})?;
 		}
 		Ok(Program {
+			db,
 			slv: slv.clone(),
 			code: output,
 			enable_stats: false,
@@ -182,6 +182,7 @@ impl Solver {
 
 /// Structure to capture the result of succesful compilation of a Model object
 pub struct Program {
+	db: CompilerDatabase,
 	slv: Solver,
 	code: NamedTempFile,
 	enable_stats: bool,
@@ -204,39 +205,6 @@ pub enum Status {
 	Err(ShackleError),
 }
 
-/// Intermediate messages emitted by shackle in processing and solving a program
-pub enum Message<'a> {
-	/// (Intermediate) solution emitted in the process
-	Solution(BTreeMap<String, Value>),
-	/// Statistical information of the shackle or solving process
-	Statistic(&'a Map<String, serde_json::Value>),
-	/// Trace messages emitted during the shackle process
-	Trace(&'a str),
-	/// Warning messages emitted by shackle or the solver
-	Warning(&'a str),
-}
-
-impl<'a> Display for Message<'a> {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match self {
-			Message::Solution(sol) => {
-				for (name, val) in sol {
-					writeln!(f, "{} = {};", name, val)?;
-				}
-				writeln!(f, "----------")
-			}
-			Message::Statistic(map) => {
-				for (name, val) in *map {
-					writeln!(f, "%%%mzn-stat: {}={}", name, val)?;
-				}
-				writeln!(f, "%%%mzn-stat-end")
-			}
-			Message::Trace(msg) => write!(f, "% mzn-trace: {}", msg),
-			Message::Warning(msg) => write!(f, "% WARNING: {}", msg),
-		}
-	}
-}
-
 /// Value types that can be part of a Solution
 pub enum Value {
 	/// Absence of an optional value
@@ -250,6 +218,7 @@ pub enum Value {
 	/// String
 	String(String),
 	/// Identifier of a value of an enumerated type
+	// FIXME this should probably have the actual structuring of enumerated types
 	Enum(String),
 	/// An array of values
 	/// All values are of the same type
@@ -350,143 +319,36 @@ impl Display for Value {
 	}
 }
 
-struct ValueVisitor;
-impl<'de> Visitor<'de> for ValueVisitor {
-	type Value = Value;
-
-	fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-		formatter.write_str("MiniZinc Value")
-	}
-	fn visit_none<E: serde::de::Error>(self) -> Result<Self::Value, E> {
-		Ok(Value::Absent)
-	}
-	fn visit_some<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
-		deserializer.deserialize_any(Self)
-	}
-	fn visit_bool<E: serde::de::Error>(self, v: bool) -> Result<Self::Value, E> {
-		Ok(Value::Boolean(v))
-	}
-	fn visit_i64<E: serde::de::Error>(self, v: i64) -> Result<Self::Value, E> {
-		Ok(Value::Integer(v))
-	}
-	fn visit_u64<E: serde::de::Error>(self, v: u64) -> Result<Self::Value, E> {
-		Ok(Value::Integer(v.try_into().unwrap()))
-	}
-	fn visit_f64<E: serde::de::Error>(self, v: f64) -> Result<Self::Value, E> {
-		Ok(Value::Float(v))
-	}
-	fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
-		Ok(Value::String(String::from(v)))
-	}
-	fn visit_seq<V: SeqAccess<'de>>(self, mut seq: V) -> Result<Self::Value, V::Error> {
-		let mut vec: Vec<Value> = Vec::with_capacity(seq.size_hint().unwrap_or(0));
-		while let Some(el) = seq.next_element()? {
-			vec.push(el);
-		}
-		Ok(Value::Tuple(vec))
-	}
-
-	fn visit_map<V: MapAccess<'de>>(self, mut map: V) -> Result<Self::Value, V::Error> {
-		let ret = match map.next_key()? {
-			Some("c") => {
-				let func: &str = map.next_value()?;
-				match map.next_key()? {
-					Some("e") => {
-						let val: Value = map.next_value()?;
-						Ok(Value::Enum(format!("{func}({val})")))
-					}
-					Some(key) => Err(serde::de::Error::unknown_field(key, &["e"])),
-					None => Err(serde::de::Error::missing_field("e")),
-				}
-			}
-			Some("i") => {
-				let val: i64 = map.next_value()?;
-				match map.next_key()? {
-					Some("e") => {
-						let name: &str = map.next_value()?;
-						Ok(Value::Enum(format!("to_enum({name}, {val})")))
-					}
-					Some(key) => Err(serde::de::Error::unknown_field(key, &["e"])),
-					None => Err(serde::de::Error::missing_field("e")),
-				}
-			}
-			Some("e") => {
-				let val = map.next_value()?;
-				match map.next_key()? {
-					Some("i") => {
-						if let Value::String(val) = val {
-							let ival: i64 = map.next_value()?;
-							Ok(Value::Enum(format!("to_enum({val}, {ival})")))
-						} else {
-							Err(serde::de::Error::custom("non-string enum identifier"))
-						}
-					}
-					Some("c") => {
-						let func: &str = map.next_value()?;
-						Ok(Value::Enum(format!("{func}({val})")))
-					}
-					Some(field) => Err(serde::de::Error::unknown_field(field, &["i", "c"])),
-					None => {
-						if let Value::String(e) = val {
-							Ok(Value::Enum(e))
-						} else {
-							Err(serde::de::Error::custom("non-string enum identifier"))
-						}
-					}
-				}
-			}
-			Some("indices") => {
-				let idx: Vec<(i64, i64)> = map.next_value()?;
-				match map.next_key()? {
-					Some("members") => {
-						let members = map.next_value()?;
-						Ok(Value::Array(
-							idx.iter().map(|r| r.0..(r.1 + 1)).collect(),
-							members,
-						))
-					}
-					Some(field) => Err(serde::de::Error::unknown_field(field, &["members"])),
-					None => Err(serde::de::Error::missing_field("members")),
-				}
-			}
-			Some("members") => {
-				let members = map.next_value()?;
-				match map.next_key()? {
-					Some("indices") => {
-						let idx = map.next_value()?;
-						Ok(Value::Array(idx, members))
-					}
-					Some(field) => Err(serde::de::Error::unknown_field(field, &["indices"])),
-					None => Err(serde::de::Error::missing_field("indices")),
-				}
-			}
-			Some("set") => {
-				// FIXME: Only works for integer sets
-				let ranges: Vec<(i64, i64)> = map.next_value()?;
-				let members = ranges
-					.iter()
-					.flat_map(|range| (range.0..=range.1))
-					.map(Value::Integer)
-					.collect();
-				Ok(Value::Set(members))
-			}
-			Some("record") => Ok(Value::Record(map.next_value()?)),
-			_ => Err(serde::de::Error::invalid_length(
-				0,
-				&"object of size 1 or 2",
-			)),
-		};
-		if let Some(key) = map.next_key()? {
-			Err(serde::de::Error::unknown_field(key, &[]))
-		} else {
-			ret
-		}
-	}
+/// Intermediate messages emitted by shackle in processing and solving a program
+pub enum Message<'a> {
+	/// (Intermediate) solution emitted in the process
+	Solution(BTreeMap<String, Value>),
+	/// Statistical information of the shackle or solving process
+	Statistic(&'a Map<String, serde_json::Value>),
+	/// Trace messages emitted during the shackle process
+	Trace(&'a str),
+	/// Warning messages emitted by shackle or the solver
+	Warning(&'a str),
 }
 
-impl<'de> Deserialize<'de> for Value {
-	fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-		deserializer.deserialize_option(ValueVisitor)
+impl<'a> Display for Message<'a> {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Message::Solution(sol) => {
+				for (name, val) in sol {
+					writeln!(f, "{} = {};", name, val)?;
+				}
+				writeln!(f, "----------")
+			}
+			Message::Statistic(map) => {
+				for (name, val) in *map {
+					writeln!(f, "%%%mzn-stat: {}={}", name, val)?;
+				}
+				writeln!(f, "%%%mzn-stat-end")
+			}
+			Message::Trace(msg) => write!(f, "% mzn-trace: {}", msg),
+			Message::Warning(msg) => write!(f, "% WARNING: {}", msg),
+		}
 	}
 }
 
@@ -500,142 +362,6 @@ impl Program {
 	pub fn with_time_limit(mut self, dur: Duration) -> Self {
 		self.time_limit = Some(dur);
 		self
-	}
-
-	/// Run the program in the current state
-	/// Solutions are emitted to the callback, and the resulting status is returned.
-	pub fn run<F: Fn(&Message) -> bool>(&mut self, msg_callback: F) -> Status {
-		let mut cmd = Command::new("minizinc");
-		cmd.stdin(Stdio::null())
-			.stdout(Stdio::piped())
-			.stderr(Stdio::null())
-			.arg(self.code.path())
-			.args([
-				"--output-mode",
-				"typed-json",
-				"--json-stream",
-				"--output-time",
-				"--output-objective",
-				"--output-output-item",
-				"--intermediate-solutions",
-				"--solver",
-				self.slv.ident.as_str(),
-			]);
-		if let Some(time_limit) = self.time_limit {
-			cmd.args(["--time-limit", time_limit.as_millis().to_string().as_str()]);
-		}
-		if self.enable_stats {
-			cmd.arg("--statistics");
-		}
-
-		let mut child = cmd.spawn().unwrap(); // TODO: fix unwrap
-		let stdout = child.stdout.take().unwrap();
-
-		let mut status = Status::Unknown;
-		for line in BufReader::new(stdout).lines() {
-			match line {
-				Err(err) => {
-					return Status::Err(
-						InternalError::new(format!("minizinc output error: {}", err)).into(),
-					);
-				}
-				Ok(line) => {
-					let obj = serde_json::from_str::<Map<String, serde_json::Value>>(&line)
-						.expect("bad message in mzn json");
-					let ty = obj["type"].as_str().expect("bad type field in mzn json");
-					match ty {
-						"statistics" => {
-							if let serde_json::Value::Object(map) = &obj["statistics"] {
-								msg_callback(&Message::Statistic(map));
-							} else {
-								return Status::Err(
-									InternalError::new(format!(
-										"minizinc invalid statistics message: {}",
-										obj["statistics"]
-									))
-									.into(),
-								);
-							}
-						}
-						"solution" => {
-							let mut sol = BTreeMap::new();
-							for (k, v) in obj["output"]["json"]
-								.as_object()
-								.expect("invalid output.json field in mzn json")
-							{
-								let val = v.deserialize_any(ValueVisitor).unwrap_or_else(|_| {
-									panic!("invalid minizinc json value: {:?}", v)
-								});
-								sol.insert(k.clone(), val);
-							}
-							msg_callback(&Message::Solution(sol));
-							if let Status::Unknown = status {
-								status = Status::Satisfied;
-							}
-						}
-						"status" => match obj["status"]
-							.as_str()
-							.expect("bad status field in mzn json")
-						{
-							"ALL_SOLUTIONS" => status = Status::AllSolutions,
-							"OPTIMAL_SOLUTION" => status = Status::Optimal,
-							"UNSATISFIABLE" => status = Status::Infeasible,
-							"UNBOUNDED" => todo!(),
-							"UNSAT_OR_UNBOUNDED" => todo!(),
-							"UNKNOWN" => status = Status::Unknown,
-							"ERROR" => {
-								status = Status::Err(
-									InternalError::new(
-										"Error occurred, but no message was provided",
-									)
-									.into(),
-								)
-							}
-							s => {
-								return Status::Err(
-									InternalError::new(format!(
-										"minizinc unknown status type: {}",
-										s
-									))
-									.into(),
-								);
-							}
-						},
-						"error" => {
-							return Status::Err(
-								InternalError::new(format!("minizinc error: {}", obj["message"]))
-									.into(),
-							);
-						}
-						"warning" => {
-							msg_callback(&Message::Warning(
-								obj["message"]
-									.as_str()
-									.expect("invalid message field in mzn json object"),
-							));
-						}
-						s => {
-							return Status::Err(
-								InternalError::new(format!("minizinc unknown message type: {}", s))
-									.into(),
-							);
-						}
-					}
-				}
-			}
-		}
-		match child.wait() {
-			Ok(code) => {
-				if !code.success() {
-					log::warn!(
-						"The MiniZinc process terminated with exit code {}",
-						code.code().unwrap()
-					)
-				};
-				status
-			}
-			Err(e) => Status::Err(InternalError::new(format!("process error: {}", e)).into()),
-		}
 	}
 }
 
