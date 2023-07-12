82d4d92c673543417526e9c5fb6478d8ddd5eaf3
Jason N <cyderize@users.noreply.github.com>
2023-07-12 10:11:57 +1000

    Add MIR documentation

--- crates/shackle/src/mir/mod.rs
+++ crates/shackle/src/mir/mod.rs
@@ -10,148 +10,223 @@ use crate::{
 
 use ty::Ty;
 
+/// A mid-level IR program (MicroZinc)
 pub struct Model {
-	entrypoint: Expression,
-	annotations: Arena<Annotation>,
-	functions: Arena<Function>,
+	_entrypoint: Expression,
+	_annotations: Arena<Annotation>,
+	_functions: Arena<Function>,
 }
 
+/// An annotation item
 pub struct Annotation {
-	name: Identifier,
-	parameter_count: u16,
+	_name: Identifier,
+	_parameter_count: u16,
 }
 
+/// A constraint item
 pub struct Constraint {
-	expression: Expression,
-	annotations: Vec<AnnotationRef>,
+	_expression: Expression,
+	_annotations: Vec<AnnotationRef>,
 }
 
+/// An annotation
 pub enum AnnotationRef {
+	/// Identifier for declaration with a RHS expression
 	Identifier(Identifier),
+	/// Direct reference to annotation definition
 	Reference(AnnotationId),
 }
 
+/// The ID of an annotation item
 pub type AnnotationId = ArenaIndex<Annotation>;
 
+/// A declaration item
 pub struct Declaration {
-	ty: Ty,
-	domain: Option<Domain>,
-	name: Identifier,
-	definition: Option<Expression>,
+	_ty: Ty,
+	_domain: Option<Domain>,
+	_name: Identifier,
+	_definition: Option<Expression>,
 }
 
+/// A domain
 pub enum Domain {
+	/// Identifier for declaration with a RHS expression
 	Identifier(Identifier),
+	/// Fully evaluated set domain
 	Set(Set),
 }
 
+/// A function item
 pub struct Function {
-	name: Identifier,
-	parameters: Vec<Identifier>,
-	body: Option<Expression>,
+	_name: Identifier,
+	_parameters: Vec<Identifier>,
+	_body: Option<Expression>,
 }
 
+/// An expression
 pub struct Expression {
-	data: ExpressionData,
-	ty: Ty,
-	origin: Origin,
+	_data: ExpressionData,
+	_ty: Ty,
+	_origin: Origin,
 }
 
+/// The expression data
 pub enum ExpressionData {
+	/// A let expression
 	Let(Let),
+	/// A call
 	Call(Call),
+	/// An if-then-else expression
 	IfThenElse(IfThenElse),
+	/// A comprehension
 	Comprehension(Comprehension),
+	/// A value
 	Value(ValueData),
+	/// A root-level forall
 	Forall(Comprehension),
 }
-pub struct Tuple {
-	pub members: Vec<Value>,
-}
-pub struct Array {
-	pub members: Vec<Value>,
-}
-pub struct Set {
-	pub members: Vec<Value>,
+
+/// A let expression
+pub struct Let {
+	_items: Vec<LetItem>,
+	_result: Option<Identifier>,
 }
 
+/// An item in a let expression
+pub enum LetItem {
+	/// A constraint
+	Constraint(Constraint),
+	/// A declaration
+	Declaration(Declaration),
+}
+
+/// A tuple literal
+pub struct Tuple {
+	/// Tuple members
+	pub members: Vec<Value>,
+}
+/// An array literal
+pub struct Array {
+	/// Array literal members
+	pub members: Vec<Value>,
+}
+/// A set literal
+pub struct Set {
+	/// Set literal members
+	pub members: Vec<Value>,
+}
+/// An array access
 pub struct ArrayAccess {
+	/// The array being indexed
 	pub array: Identifier,
+	/// The indices being used to index the array
 	pub indices: Vec<Literal>,
 }
 
+/// A tuple field access
 pub struct TupleAccess {
+	/// The tuple being accessed
 	pub tuple: Identifier,
+	/// The field
 	pub field: IntegerLiteral,
 }
 
+/// A call
 pub struct Call {
+	/// The function being called
 	pub function: Identifier,
+	/// The arguments
 	pub arguments: Vec<Value>,
 }
 
+/// An if-then-else expression
+///
+/// This only has an if-then and else branch, so may need to be nested
 pub struct IfThenElse {
+	/// The (par) condition
 	pub condition: Value,
+	/// The value if the condition holds
 	pub then: Box<Expression>,
+	/// The value if the condition doesn't hold
 	pub else_expression: Box<Expression>,
 }
 
+/// A comprehension
 pub struct Comprehension {
+	/// The indices of the generated expression
 	pub indices: Option<Box<Expression>>,
+	/// The generated expression
 	pub expression: Box<Expression>,
+	/// The generators
 	pub generators: Vec<Generator>,
 }
 
+/// A generator in a comprehension
 pub enum Generator {
+	/// An iterator such as `i, j in foo where bar`
 	Iterator {
+		/// The names of the iterators
 		names: Vec<Identifier>,
+		/// The collection being iterated over
 		collection: Expression,
+		/// The where clause
 		where_clause: Option<Expression>,
 	},
+	/// An assignment such as `i = foo where bar`
 	Assignment {
+		/// The name of the assignment
 		name: Identifier,
+		/// The value of the assignment
 		definition: Expression,
+		/// The where clause
 		where_clause: Option<Expression>,
 	},
 }
 
+/// A literal
 pub struct Literal {
-	data: LiteralData,
-	ty: Ty,
-	origin: Origin,
+	_data: LiteralData,
+	_ty: Ty,
+	_origin: Origin,
 }
 
+/// The literal data
 pub enum LiteralData {
+	/// Bottom (cannot be evaluated)
 	Bottom,
+	/// A boolean
 	Boolean(BooleanLiteral),
+	/// An integer
 	Integer(IntegerLiteral),
+	/// A floating point value
 	Float(FloatLiteral),
+	/// A string
 	String(StringLiteral),
+	/// Infinity
 	Infinity,
+	/// An identifier
 	Identifier(Identifier),
 }
 
+/// A value
 pub struct Value {
-	data: ValueData,
-	ty: Ty,
-	origin: Origin,
+	_data: ValueData,
+	_ty: Ty,
+	_origin: Origin,
 }
 
+/// The value data
 pub enum ValueData {
+	/// A literal
 	Literal(LiteralData),
+	/// A tuple
 	Tuple(Tuple),
+	/// A set
 	Set(Set),
+	/// An array
 	Array(Array),
+	/// An array access
 	ArrayAccess(ArrayAccess),
+	/// A tuple access
 	TupleAccess(TupleAccess),
-}
-
-pub struct Let {
-	items: Vec<LetItem>,
-	result: Option<Identifier>,
-}
-
-pub enum LetItem {
-	Constraint(Constraint),
-	Declaration(Declaration),
 }
--- crates/shackle/src/mir/ty.rs
+++ crates/shackle/src/mir/ty.rs
@@ -1,12 +1,58 @@
+//! Module containing mid-level IR type representation
 use std::rc::Rc;
 
+/// A mid-level IR type
 #[allow(variant_size_differences)]
 pub enum Ty {
-	Bottom { dim: u8, is_set: bool },
-	Bool { dim: u8, is_var: bool, is_set: bool },
-	Int { dim: u8, is_var: bool, is_set: bool },
-	Float { dim: u8, is_var: bool, is_set: bool },
-	String { dim: u8 },
-	Ann { dim: u8 },
-	Tuple(Rc<[Ty]>),
+	/// Type of bottom
+	Bottom {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+		/// Whether this is a set
+		is_set: bool,
+	},
+	/// Type of a boolean
+	Bool {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+		/// Whether this is a decision variable
+		is_var: bool,
+		/// Whether this is a set
+		is_set: bool,
+	},
+	/// Type of an integer
+	Int {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+		/// Whether this is a decision variable
+		is_var: bool,
+		/// Whether this is a set
+		is_set: bool,
+	},
+	/// Type of a float
+	Float {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+		/// Whether this is a decision variable
+		is_var: bool,
+		/// Whether this is a set
+		is_set: bool,
+	},
+	/// Type of a string
+	String {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+	},
+	/// Type of an annotation
+	Ann {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+	},
+	/// Type of a tuple
+	Tuple {
+		/// Dimensions (0 if not an array)
+		dim: u8,
+		/// Types of the fields
+		fields: Rc<[Ty]>,
+	},
 }
