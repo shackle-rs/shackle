6cc0cb710aed2f32e5992718fa73114a89ab6fa2
cyderize <cyderize@users.noreply.github.com>
2023-07-20 23:08:36 +0000

    deploy: 6364d95fca2283eeda60c1e03ac0c435504c78a0

--- theme/mzn-highlight.js
+++ theme/mzn-highlight.js
@@ -1,38 +1,38 @@
 hljs.registerLanguage("MiniZinc", (hljs) => {
-  const KEYWORDS = {
-    keyword:
-      "ann annotation any array bool case constraint default div diff else elseif endif enum float function if include intersect in int let maximize minimize mod not of output opt par predicate record satisfy set solve string subset superset symdiff test then tuple type union var variant_record where xor",
-    literal: "false true <> _",
-  };
-  const STRING = {
-    scope: "string",
-    begin: '"',
-    end: '"',
-    contains: [hljs.BACKSLASH_ESCAPE],
-  };
-  const NUMBER = {
-    className: "number",
-    begin: "\\b0x[\\da-f]+\\b|(?:\\b\\d+\\.?\\d*|\\B\\.\\d+)(?:e[+-]?\\d+)?",
-  };
-  const SUBST = {
-    className: "subst",
-    begin: "\\\\\\(",
-    end: "\\)",
-    keywords: KEYWORDS,
-    contains: [STRING, NUMBER],
-  };
-  STRING.contains.push(SUBST);
-  return {
-    name: "MiniZinc",
-    aliases: ["mzn", "dzn", "fzn"],
-    keywords: KEYWORDS,
-    contains: [
-      STRING,
-      NUMBER,
-      hljs.COMMENT("/\\*", "\\*/"),
-      hljs.COMMENT("%", "$"),
-    ],
-  };
-});
+	const KEYWORDS = {
+		keyword:
+			"ann annotation any array bool case constraint default div diff else elseif endcase endif enum float function if include intersect in int lambda let maximize minimize mod not of op opt output par predicate record satisfy set solve string subset superset symdiff test then tuple type union var variant_record where xor",
+		literal: "false true <> _",
+	}
+	const STRING = {
+		scope: "string",
+		begin: '"',
+		end: '"',
+		contains: [hljs.BACKSLASH_ESCAPE],
+	}
+	const NUMBER = {
+		className: "number",
+		begin: "\\b0x[\\da-f]+\\b|(?:\\b\\d+\\.?\\d*|\\B\\.\\d+)(?:e[+-]?\\d+)?",
+	}
+	const SUBST = {
+		className: "subst",
+		begin: "\\\\\\(",
+		end: "\\)",
+		keywords: KEYWORDS,
+		contains: [STRING, NUMBER],
+	}
+	STRING.contains.push(SUBST)
+	return {
+		name: "MiniZinc",
+		aliases: ["mzn", "dzn", "fzn"],
+		keywords: KEYWORDS,
+		contains: [
+			STRING,
+			NUMBER,
+			hljs.COMMENT("/\\*", "\\*/"),
+			hljs.COMMENT("%", "$"),
+		],
+	}
+})
 
-hljs.initHighlightingOnLoad();
+hljs.initHighlightingOnLoad()
